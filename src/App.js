e210e62e9791c84cb31bf21cd483977cc606df2a
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-10 21:44:07 -0400

    Agregando estilos con styled-components al componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin-inline: auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
-  return <Header titulo="Cotizador de Seguros" />;
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
+  );
 }
 
 export default App;
